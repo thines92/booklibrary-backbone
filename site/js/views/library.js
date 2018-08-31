88bfde64cddaee22af6f9e9f0ce8c3fdb448b47e
Tyler <tylerhines1992@gmail.com>
2018-08-31 12:05:05 -0400

    wired the interface to the collection

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -6,9 +6,29 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
 	el: '#books',
 
+	events: {
+		'click #add': 'addBook'
+	},
+
+	addBook: function(e) {
+		e.preventDefault();
+
+		var formData = {};
+
+		$('#addBook div').children('input').each(function( i, el ) {
+			if( $(el).val() !== '' ) {
+				formData[ el.id ] = $(el).val();
+			}
+		});
+
+		this.collection.add(new app.Book(formData));
+	},
+
 	initialize: function(initialBooks) {
 		this.collection = new app.Library(initialBooks);
 		this.render();
+
+		this.listenTo( this.collection, 'add', this.renderBook );
 	},
 
 	// render library by rendering each book in its collectionb
